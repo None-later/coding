0698a1d03b078ef197366923cdd91ed536968b85
GaryYang <hsnudrum@gmail.com>
2016-01-16 19:33:43 -0500

    cc

---
+++ CppTest/CodilityPractice/ThreePartitions.h
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+int minPartition(vector<int> ivec)
+{
+	if (ivec.size() == 5)
+		return ivec[1] + ivec[3];
+
+	int x = 1;
+	int y = 3;
+
+
+
+	return 0;
+}
