00dcbce55fb5bd34b7468563be1d418f9eea2475
GaryYang <hsnudrum@gmail.com>
2016-01-18 17:02:13 -0500

    cc

--- CppTest/CodilityPractice/Real_PhoneNumber.h
+++ CppTest/CodilityPractice/Real_PhoneNumber.h
@@ -1,5 +1,33 @@
-char* FormatPhoneNumber(char* s)
-{
-	
+#include <string>
 
+using namespace std;
+
+string FormatPhoneNumber(char* s)
+{
+	string res("");
+	int count = 0;
+	for (int i = 0; i < strlen(s); i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res += s[i];
+			count++;
+			if (count == 3)
+			{
+				res+= '-';
+				count = 0;
+			}
+		}
+	}
+	if (count == 0)
+	{
+		res = res.substr(0, res.size()-1);
+	}
+	if (count == 1)
+	{
+		char c = res[res.size()-2];
+		res[res.size()-2] = res[res.size()-3];
+		res[res.size()-3] = c;
+	}
+	return res;
 }
--- CppTest/CodilityPractice/main.cpp
+++ CppTest/CodilityPractice/main.cpp
@@ -5,8 +5,128 @@
 #include "NumberSolitaire.h"
 #include "Real_ParitionThree.h"
 #include "Real_StackOperation.h"
+#include "Real_PhoneNumber.h"
 
 using namespace std;
+
+int getValue(int i)
+{
+    if (i == 0) return 0;
+    int count = 1;
+    int res = 0;
+    while (count <= i)
+    {
+        res += res + count;
+        count++;    
+    }
+    return res;
+}
+#include <set>
+string increament(string& s)
+{
+    string res = s;    
+    int adv = 0;
+    if (s[7] < '9')
+    {
+        res[7] = char(res[7] + 1);
+        return res;
+    }   
+    if (s[7] == '9')
+    {
+        res[7] = '0';
+        adv = 1;
+    }
+    if (s[6] < '5' && adv)
+    {       
+        res[6] = char(res[6] + 1);
+        return res;
+    }
+    if (s[6] == '5' && adv)
+    {
+        res[6] = '0';
+	}
+    if (s[4] < '9' && adv)
+    {       
+        res[4] = char(res[4] + 1);
+        return res;
+    }
+    if (s[4] == '9' && adv)
+    {
+        res[4] = '0';
+	}
+    if (s[3] < '5' && adv)
+    {       
+        res[3] = char(res[3] + 1);
+        return res;
+    }
+    if (s[3] == '5' && adv)
+    {
+        res[3] = '0';
+	}
+
+	if (s[0] == '1')
+	{
+		if (s[1] < '9' && adv)
+		{       
+			res[1] = char(res[1] + 1);
+			return res;
+		}
+		if (s[1] == '9' && adv)
+		{
+			res[1] = '0';
+			res[0] == '2';
+			return res;
+		}
+	}
+	else 
+	{
+		if (s[1] < '3' && adv)
+		{
+			res[1] = char(res[1] + 1);		
+		}
+	}
+    return res;
+}
+
+bool isTwoDigits(string& s)
+{
+	string digits("");
+	digits += s[0];
+	digits += s[1];
+	digits += s[3];
+	digits += s[4];
+	digits += s[6];
+	digits += s[7];
+	set<int> set;
+	set.insert(digits[0]);
+	for (int i = 1; i < digits.size(); ++i)
+	{
+		if (digits[i] != digits[i-1])
+		{
+			set.insert(digits[i]);
+			if (set.size() > 2)
+				return false;
+		}
+	}
+	return true;
+}
+
+int solution1(string &S, string &T) 
+{
+    if (S == T) return 0;
+    
+    int res = 0;
+    
+    while(S != T)
+    {
+        string s = increament(S);
+        if (isTwoDigits(s)) 
+            res++;
+        S = s;        
+    }
+    return res;
+}
+
 
 int main () 
 {
@@ -40,12 +160,28 @@ int main ()
 	//cout << PartitionThree(ivec) << endl;
 
 	
+	string s = "00:00:59";
+	cout << s[7] << endl;
 
 	//char s[] = "13+62*7";
-	
+	//char c[] = "0 - 22 1985--324";
+	//char c1[] = "00-44  48 5555 8361";
+	//cout << FormatPhoneNumber(c1) << endl;
 
-	cout << StackOperation("13+62*7+*") << endl;
+	//cout << StackOperation("13+62*7+*") << endl;
+	cout << (int)"59" << endl;
 
+	cout << getValue(3) << endl;
+
+	//cout << (char)((int)('6' - '0') + 1) << endl;
+	//cout << static_cast<char>(((int)('6' - '0') + 1) - '0') << endl;
+	string str = "13:59:59";
+	int i = '3' + 1;
+
+	cout << char('0'+1) << endl;
+	string s1 = "15:15:10";
+	string s2 = "15:15:12";
+	cout << solution1(s1, s2) << endl;
 	getchar();
 	return 0;
 }
